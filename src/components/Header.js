8b7c17750f5c192bfef25b75164313cd56a5171b
aroomforimprovement <seamusart.bradley@gmail.com>
2021-11-01 16:46:50 +0000

    header nav and auth btns

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useMainContext } from './MenurRouter';
+import { Loading } from './partials/Loading';
+import { LoginBtn, LogoutBtn, SignupBtn } from './partials/AuthBtns';
 
 export const Header = () => {
+    const { state } = useMainContext();
+    const logo = process.env.REACT_APP_URL + '/assets/site-logo.svg'
+
+    useEffect(() => {
+
+    },[state]);
     return(
-        <div className='shadow-sm bg-light mt-2'>
-            <h5 className='mb-2'>Menur</h5>
+        <div className='nav-area col-12'>
+            <Navbar collapseOnSelect expand='md' bg='light' variant='light'>
+                <Container>
+                <Navbar.Brand className='mr-auto' href='/'>
+                        <img src={logo}  height='40px' width='40px'
+                            alt='Menur Home'/>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls={'responsive-navbar-nav'}/>
+                    <Navbar.Collapse id={'responsive-navbar-nav'}>
+                        <Nav className='me-auto col col-10 col-md-9' navbar>
+                            <Nav.Link className='nav-link m-1' to='/'>
+                                    MealPlanner
+                            </Nav.Link>
+                            {!state ? <Loading /> : state.user && state.user.isAuth 
+                            ? <Nav.Link className='nav-link m-1' to={`/account`}>
+                                    Account
+                              </Nav.Link> 
+                            : <div></div>}
+                        </Nav>
+                        <div className='header bg-light mt-2'>
+                            <h5 className='mb-2'>Menur</h5>
+                        </div>
+                        <div className='auth-nav-wrapper'>
+                            {!state ? <Loading /> :
+                            <Nav className='auth-nav' navbar>
+                                <Navbar.Text className='nav-item nav-i'>
+                                    {!state || (state && state.user && state.user.isAuth) ? null : <LoginBtn size='btn-sm mt-2' />}
+                                </Navbar.Text>
+                                <Navbar.Text className='nav-item nav-i'>
+                                    {!state || (state && state.user && state.user.isAuth) ? <LogoutBtn size='btn-sm mt-2' /> : null}
+                                </Navbar.Text>
+                                <Navbar.Text className='nav-item nav-i'>
+                                    {!state || (state && state.user && state.user.isAuth) ? null : <SignupBtn size='btn-sm mt-2' />}
+                                </Navbar.Text>
+                            </Nav>
+                            }
+                        </div>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
-        
     );
 }
 
