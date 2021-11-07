cce33d9e5d2a345b7a1deac479a64089e47d7d7c
aroomforimprovement <seamusart.bradley@gmail.com>
2021-11-07 20:30:54 +0000

    cors

--- src/components/MenurRouter.js
+++ src/components/MenurRouter.js
@@ -68,6 +68,7 @@ const MenurRouter = () => {
             return await fetch(`${apiUrl}app/login`, {
                 method: 'POST',
                 body: JSON.stringify(body),
+                mode: 'cors',
                 headers: {
                     Authorization: `Bearer ${access}`,
                     'Content-Type': 'application/json'
