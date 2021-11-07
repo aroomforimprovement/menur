531a07d70b7f2fb7263ed069004a0c68d4081b16
aroomforimprovement <seamusart.bradley@gmail.com>
2021-11-07 18:24:09 +0000

    entrypoint yarn

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -eu
 
 # envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 #sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
-#yarn start
-nginx -g 'daemon off;'
+yarn start
+#nginx -g 'daemon off;'
 
 exec "$@"
