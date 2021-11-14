bdf04c186ff844c0f9aff8a22a7941eb9c21e14c
aroomforimprovement <seamusart.bradley@gmail.com>
2021-11-14 00:53:11 +0000

    help page

--- src/help/Help.js
+++ src/help/Help.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './help.css';
 import { PrivacyPolicy } from './PrivacyPolicy';
 
 export const Help = () => {
     return(
-        <div>
-            <PrivacyPolicy />
+        <div className='help-page'>
+            <div className='p-5 jum'>
+                <h1>What is Menur?</h1>
+                <p></p>
+            </div>
+            <div className='jum '>
+                <PrivacyPolicy />
+            </div>
         </div>
     );
 }
