042306cfe0e5f7450db072a052645a488abc68c9
aroomforimprovement <seamusart.bradley@gmail.com>
2021-11-15 00:12:10 +0000

    problem link

--- src/common/Problem.js
+++ src/common/Problem.js
@@ -5,7 +5,7 @@ export const Problem = ({message}) => {
         <div className='px-4 py-5 my-5 text-center'>
             <img src='/assets/fail.svg' className='d-block mx-auto mb-4 col-2' alt='error' />
             <h4>{message}</h4>
-            <button className={'btn btn-primary m-5 p-2'} href='/planner'>Take me home!</button>
+            <button className={'btn btn-primary m-5 p-2'} onClick={() => {window.location.href = './planner'}}>Take me home!</button>
         </div>
     );
 }
