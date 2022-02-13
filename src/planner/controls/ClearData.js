89e783f69cd80286049a620ed2960889afb2c20a
aroomforimprovement <seamusart.bradley@gmail.com>
2022-02-13 19:18:49 +0000

    MENUR-130/confirm dialogue - mealplan clear / generate list

--- src/planner/controls/ClearData.js
+++ src/planner/controls/ClearData.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import toast from 'react-hot-toast';
+import { ToastConfirm, toastConfirmStyle } from '../../common/Toasts';
 import { useMainContext } from '../../main/MenurRouter';
 
 export const ClearData = () => {
@@ -6,7 +8,19 @@ export const ClearData = () => {
     const {dispatch} = useMainContext();
 
     const handleClearData = () => {
-        dispatch({type: 'CLEAR_DATA', data: true});
+        const setIsCancelled = (id) => {
+            toast.dismiss(id);
+        }
+        const setDataCleared = (id) => {
+            toast.dismiss(id);
+            dispatch({type: 'CLEAR_DATA', data: true});
+            toast.success("You're ready to start fresh!");
+        }
+        toast((t) => (
+            <ToastConfirm t={t} approve={setDataCleared} dismiss={setIsCancelled}
+                message={'Are you sure you want to clear all data from this screen and start again?'}
+                approveBtn={'Clear data'} dismissBtn={'Cancel'} />
+        ), toastConfirmStyle());
     }
 
     return(
--- src/planner/controls/GenerateList.js
+++ src/planner/controls/GenerateList.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { toast } from 'react-hot-toast';
+import { ToastConfirm, toastConfirmStyle } from "../../common/Toasts";
 import { useMainContext } from '../../main/MenurRouter';
 
 export const GenerateList = () => {
     const {dispatch} = useMainContext();
-    
+
     const handleGenList = () => {
-        dispatch({type: 'GEN_LIST', data:true});
+        const setIsCancelled = (id) => {
+            toast.dismiss(id);
+            dispatch({type: 'GEN_LIST', data:true});
+            toast.success("Shopping list generated");
+        }
+        toast((t) => (
+            <ToastConfirm t={t} approve={setIsCancelled} dismiss={setIsCancelled}
+                message={`FYI - If you change anything in the mealplan, it won't update the shopping list.
+                    You'll have to use this button again and generate a new shopping list.`}
+                approveBtn={'OK'} dismissBtn={'Cool'}  />
+        ), toastConfirmStyle());
     }
 
     return (
         <div>
             <div className='mt-2 mb-2'>
-
                     <button onClick={handleGenList} 
                         className='butt butt-warn border col col-11 mx-auto shadow'>
                         <span className='fa fa-list me-2'></span>Generate shopping list<span className='fa fa-list ms-2'></span>
