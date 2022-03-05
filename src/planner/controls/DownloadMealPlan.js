a15d4a48540abd776c159f46876963d6d73ff33a
aroomforimprovement <seamusart.bradley@gmail.com>
2022-03-05 19:49:16 +0000

    MENUR-163/option 1, download without done

--- src/planner/controls/DownloadMealPlan.js
+++ src/planner/controls/DownloadMealPlan.js
@@ -3,16 +3,36 @@ import { pdf } from '@react-pdf/renderer';
 import { saveAs } from 'file-saver';
 import { useMainContext } from '../../main/MenurRouter';
 import { DownloadableMealPlan, DownloadableMealPlanLandscape } from '../../utils/pdfUtils';
+import toast from 'react-hot-toast';
+import { toastConfirmStyle, ToastOptions } from '../../common/Toasts';
 
 export const DownloadMealPlan = () => {
     const { state } = useMainContext();
 
+
     const handleDownload = async () => {
-        const blob = await pdf(
-            state.isLandscape 
-            ? DownloadableMealPlanLandscape(state.mealplan)
-            : DownloadableMealPlan(state.mealplan)).toBlob();
-        saveAs(blob, `Menur Plan - ${state.mealplan.name ? state.mealplan.name : Date.now()}`)
+        const cancel = (id) => {
+            toast.dismiss(id);
+        }
+        const downloadWithShoppingList = (id) => {
+            console.log("with");
+            
+            toast.dismiss(id);
+        }
+        const downloadWithoutShoppingList = async (id) => {
+            const blob = await pdf(
+                state.isLandscape 
+                ? DownloadableMealPlanLandscape(state.mealplan)
+                : DownloadableMealPlan(state.mealplan)).toBlob();
+            saveAs(blob, `Menur Plan - ${state.mealplan.name ? state.mealplan.name : Date.now()}`)
+            toast.dismiss(id);
+        }
+
+        toast((t) => (
+            <ToastOptions t={t} dismiss={cancel} options={[downloadWithShoppingList, downloadWithoutShoppingList]}
+            optionBtns={["With", "Without"]} dismissBtn={'Cancel'} 
+            message={'Would you like to download the meal plan with your shopping list(s) attached?'} />
+        ), toastConfirmStyle());
     }
 
     return(
