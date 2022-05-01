8cab68f1255b653dd6573483797dbada30fd868b
aroomforimprovement <seamusart.bradley@gmail.com>
2022-05-01 02:48:26 +0100

    adjusted mealplanslot css

--- src/planner/mealplan/MealPlanSlot.js
+++ src/planner/mealplan/MealPlanSlot.js
@@ -89,14 +89,15 @@ export const MealPlanSlot = ({mealtime, day}) => {
             onDragEnter={handleDragEnter} onDragLeave={handleDragLeave} onHit={handleDrop}>                
             <div className={`mealtime border shadow shadow-sm ${state.isLandscape ? 'mealtime-ls' : 'mealtime-pt'} 
                 ${hasHighlightedIngredient ? 'border-success' : ''}`} >
+                <div onClick={setMealtimePickerOpen} className='mealtime-click fa fa-edit'>
+                       
+                       </div>
                 <DragDropContainer targetKey='meal' onDrop={handleDropOver} dragData={{meal: state.mealplan[day][mealtime]}}>
                     <div className='mealtime-text'>{state.mealplan[day][mealtime].name 
                         ? state.mealplan[day][mealtime].name  : ' '}
                     </div>
                 </DragDropContainer>
-                <div onClick={setMealtimePickerOpen} className='mealtime-click fa fa-edit'>
-                       
-                </div>
+                
                 <div>
             </div>
             <div className={'row'}> 
